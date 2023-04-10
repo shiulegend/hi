783dd7fee06b3fcc040666ed93d2eb4130749a82
shiulegend <shiulegend@gmail.com>
2023-04-10 19:12:21 +0800

    add file

---
+++ 123.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr  7 20:36:25 2023
+
+@author: shiul
+"""
+for i in range(1, 10):
+    for j in range(1, 10):
+        print("%d * %d = %d" % (i, j, i*j))
+    print()
